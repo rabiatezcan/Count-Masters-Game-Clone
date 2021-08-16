bbcf045a4a6756678b6f6f83a7de9aa5e4925282
rabiatezcan <rabiatezcan@hotmail.com>
2021-08-16 13:20:19 +0300

    feat : added camera follow controls

---
+++ Count Masters/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float cameraOffset = 8f;
+    private GameObject player; 
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y,
+            player.transform.position.z - cameraOffset);
+    }
+}
