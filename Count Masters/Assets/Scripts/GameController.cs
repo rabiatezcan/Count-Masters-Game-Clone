31c6ab5056a465a706e022d282396460f32a7aa4
rabiatezcan <rabiatezcan@hotmail.com>
2021-08-18 01:48:09 +0300

    fix : instantiation converted to the function

--- Count Masters/Assets/Scripts/GameController.cs
+++ Count Masters/Assets/Scripts/GameController.cs
@@ -7,15 +7,29 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
     private Vector3 deltaVector = new Vector3(0.35f, 0, 0);
-
+    private GameObject player;
     private void Awake()
     {
-        Instantiate(playerPrefab, gameObject.transform.position, Quaternion.identity);
-        Instantiate(playerPrefab, gameObject.transform.position + deltaVector , Quaternion.identity);
+        SpawnPlayer(2);
+    }
+
+    private void Start()
+    {
+        
     }
 
     void Update()
     {
         
+    }
+
+    public void SpawnPlayer(int playerCount)
+    {
+        player = FindObjectOfType<PlayersDragController>().gameObject;
+        
+        for (int i = 0; i < playerCount; i++)
+        {
+            Instantiate(playerPrefab, player.transform.position + (deltaVector * i) , Quaternion.identity);
+        }
     }
 }
