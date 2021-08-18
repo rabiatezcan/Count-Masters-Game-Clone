24979da04b5b908e0446203de3e5c869aa85d78d
rabiatezcan <rabiatezcan@hotmail.com>
2021-08-18 14:33:05 +0300

    feat : access to first child of players for the instantiation position

--- Count Masters/Assets/Scripts/GameController.cs
+++ Count Masters/Assets/Scripts/GameController.cs
@@ -8,28 +8,30 @@ public class GameController : MonoBehaviour
     [SerializeField] private GameObject playerPrefab;
     private Vector3 deltaVector = new Vector3(0.35f, 0, 0);
     private GameObject player;
+
     private void Awake()
     {
+        player = FindObjectOfType<PlayersDragController>().gameObject;
         SpawnPlayer(2);
     }
 
     private void Start()
     {
-        
     }
 
     void Update()
     {
-        
     }
 
     public void SpawnPlayer(int playerCount)
     {
-        player = FindObjectOfType<PlayersDragController>().gameObject;
-        
+        if (player.transform.childCount > 1)
+        {
+            player = player.transform.GetChild(0).gameObject;
+        }
         for (int i = 0; i < playerCount; i++)
         {
-            Instantiate(playerPrefab, player.transform.position + (deltaVector * i) , Quaternion.identity);
+            Instantiate(playerPrefab, player.transform.position + (deltaVector * i), Quaternion.identity);
         }
     }
 }
