5a3960846d0cb9bf49fb679341cb70a2f625d390
rabiatezcan <rabiatezcan@hotmail.com>
2021-08-19 17:27:31 +0300

    feat : added ui functions

--- Count Masters/Assets/Scripts/GameController.cs
+++ Count Masters/Assets/Scripts/GameController.cs
@@ -2,17 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-
+    [SerializeField] private GameObject levelFinishPanel;
+    [SerializeField] private GameObject gameOverPanel;
+    private PlayerManager playerManager;
+    
     private void Start()
     {
+        playerManager = FindObjectOfType<PlayerManager>();
     }
 
     void Update()
     {
+        if (playerManager.isGameOver)
+        {
+            gameOverPanel.SetActive(true);
+        }
         
     }
-    
+
+    public void GoToScene(int value)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + value);
+    }
+
 }
